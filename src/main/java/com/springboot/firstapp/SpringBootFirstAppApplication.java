41c163d9af9285839bed99f433b05a6504d7c6b8
CodeCrafter <ivan.turchyn94@gmail.com>
2025-01-06 20:16:51 +0100

    Section 8. Spring Boot - Fundamentals

--- src/main/java/com/springboot/firstapp/SpringBootFirstAppApplication.java
+++ src/main/java/com/springboot/firstapp/SpringBootFirstAppApplication.java
@@ -2,12 +2,18 @@ package com.springboot.firstapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootFirstAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootFirstAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(SpringBootFirstAppApplication.class, args);
+
+        String[] beanNames = applicationContext.getBeanDefinitionNames();
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+    }
 
 }
